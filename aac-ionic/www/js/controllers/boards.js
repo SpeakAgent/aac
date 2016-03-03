858724422f406a489fe26358042b8bd4b8b8fc89
Freetown5 <iconteh21@gmail.com>
2016-03-03 15:12:44 -0500

    got the colored svgs to show

--- aac-ionic/www/js/controllers/boards.js
+++ aac-ionic/www/js/controllers/boards.js
@@ -45,18 +45,65 @@ app.controller('BoardController', function($http, $scope, $ionicSideMenuDelegate
   // }
 
   $scope.colorName =[
-    {colorTitle: 'Sky Blue'},
-    {colorTitle: 'Electric Green'},
-    {colorTitle: 'Hot Pink'},
-    {colorTitle: 'Tangerine'},
-    {colorTitle: 'Butter Yellow'},
-    {colorTitle: 'Tomato Red'},
-    {colorTitle: 'Denim Blue'},
-    {colorTitle: 'Steel Gray'},
-    {colorTitle: 'Periwinkle Blue'},
-    {colorTitle: 'Forest Green'},
-    {colorTitle: 'Intense Purple'},
-    {colorTitle: 'Seafoam Green'},
+    {colorTitle: 'Sky Blue',
+     primaryColor:'#50E2E3',
+     secondaryColor:'#008484',
+     url:'img/color_change/colorBlob-skyBlue.svg' },
+
+    {colorTitle: 'Electric Green',
+     primaryColor:'#BCE72B', 
+     secondaryColor:'#18745C', 
+     url:'img/color_change/colorBlob_electricGreen.svg' },
+
+    {colorTitle: 'Hot Pink',
+     primaryColor:'#D5388A',
+     secondaryColor:'#F787C6', 
+     url:'img/color_change/colorBlob_hotPink.svg' },
+
+    {colorTitle: 'Tangerine',
+     primaryColor:'#E07600',
+     secondaryColor:'#982900', 
+     url:'img/color_change/colorBlob_tangerine.svg' },
+
+    {colorTitle: 'Butter Yellow',
+     primaryColor:'#FFDB3B',
+     secondaryColor:'DEC75F',
+     url:'img/color_change/colorBlob_butterYellow.svg' },
+
+    {colorTitle: 'Tomato Red',
+     primaryColor:'#E6213F',
+     secondaryColor:'#E899A6',
+     url:'img/color_change/colorBlob_tomatoRed.svg' },
+
+    {colorTitle: 'Denim Blue',
+     primaryColor:'#325DC1',
+     secondaryColor:'#ADB1E8',
+     url:'img/color_change/colorBlob_denimBlue.svg' },
+
+    {colorTitle: 'Steel Gray',
+     primaryColor:'#7D989A',
+     secondaryColor:'#A9CED1',
+     url:'img/color_change/colorBlob_steelGray.svg' },
+
+    {colorTitle: 'Periwinkle Blue',
+     primaryColor:'#8AB6E1',
+     secondaryColor:'#3496C7', 
+     url:'img/color_change/colorBlob_periwinkleBlue.svg' },
+
+    {colorTitle: 'Forest Green',
+     primaryColor:'#18745C',
+     secondaryColor:'#7BB59F',
+     url:'img/color_change/colorBlob_forestGreen.svg'  },
+
+    {colorTitle: 'Intense Purple',
+     primaryColor:'#6B28C6',
+     secondaryColor:'#C6A4EB',
+     url:'img/color_change/colorBlob_intensePurple.svg'  },
+
+    {colorTitle: 'Seafoam Green',
+     primaryColor:'#2FCB95',
+     secondaryColor:'#A7E8C5',
+     url:'img/color_change/colorBlob_seafoamGreen.svg'  },
   ]
   
 
@@ -84,8 +131,7 @@ app.controller('BoardController', function($http, $scope, $ionicSideMenuDelegate
   }
 
   $scope.dummyBoards =[
-  { 
-    name:"Anmls",
+  { name:"Anmls",
     img_path:"img/aac_board_imgs/alpaca.png" },
 
   { name:"Arts",
