f7de1585b3a18cd3d8ca39252c0992f1cbdbbe0a
Freetown5 <iconteh21@gmail.com>
2016-09-22 16:30:11 -0400

    considering spliting board controller file into multiple controllers

--- aac-ionic/www/js/controllers/boardFactoryCtrl.js
+++ aac-ionic/www/js/controllers/boardFactoryCtrl.js
@@ -15,161 +15,6 @@ app.controller('boardFactoryController',
 		$scope.end = 24;
 		$scope.board = {};
 
-		$scope.dummyBoards =[
-
-			{ name:"About Me",
-			  img_path:"img/aac_board_imgs/crayon.png",
-			  pk: '5'},
-
-		  { name:"Lunch",
-		    img_path:"img/aac_board_imgs/alpaca.png",
-		    pk: '3'},
-
-		  { name:"Feelings",
-		    img_path:"img/aac_board_imgs/art.png",
-		    pk: '4'},
-
-		  { name:"Body",
-		    img_path:"img/aac_board_imgs/balloon.png"},
-
-		  { name:"Clothes",
-		   	img_path:"img/aac_board_imgs/bird.png"},
-
-			{ name:"Cars",
-			  img_path:"img/aac_board_imgs/clock.png"},
-
-		  { name:"Feelings",
-		 		img_path:"img/aac_board_imgs/crayon.png"},
-
-			{ name:"Food",
-		    img_path:"img/aac_board_imgs/alpaca.png"},
-
-		  { name:"Kind Things",
-		    img_path:"img/aac_board_imgs/art.png"},
-
-		  { name:"Morning",
-		    img_path:"img/aac_board_imgs/balloon.png"},
-
-		  { name:"Number",
-		    img_path:"img/aac_board_imgs/bird.png"},
-
-			{ name:"People",
-		    img_path:"img/aac_board_imgs/clock.png"},
-
-		  { name:"Places",
-		    img_path:"img/aac_board_imgs/crayon.png"},
-
-		  { name:"Reading",
-		    img_path:"img/aac_board_imgs/alpaca.png"},
-
-		  { name:"Sports",
-		    img_path:"img/aac_board_imgs/art.png"},
-
-			{ name:"Things",
-		    img_path:"img/aac_board_imgs/balloon.png"},
-
-		  { name:"Time",
-		    img_path:"img/aac_board_imgs/bird.png"},
-
-		  { name:"Verbs",
-		    img_path:"img/aac_board_imgs/clock.png"},
-
-		  { name:"Weather",
-		    img_path:"img/aac_board_imgs/crayon.png"},
-
-			{ name:"Nouns",
-		    img_path:"img/aac_board_imgs/alpaca.png"},
-
-		  { name:"Outdoor",
-		    img_path:"img/aac_board_imgs/art.png"},
-
-		  { name:"Animals",
-		    img_path:"img/aac_board_imgs/balloon.png"},
-
-		  { name:"Plants",
-		    img_path:"img/aac_board_imgs/bird.png"},
-
-		  { name:"Stuff",
-		    img_path:"img/aac_board_imgs/clock.png"},
-
-		  { name:"Things",
-		    img_path:"img/aac_board_imgs/crayon.png"},
-
-		  // added for sliding option
-		  { name:"Nouns",
-		    img_path:"img/aac_board_imgs/alpaca.png"},
-
-		  { name:"Outdoor",
-		    img_path:"img/aac_board_imgs/art.png"},
-
-		  { name:"Animals",
-		    img_path:"img/aac_board_imgs/balloon.png"},
-
-		  { name:"Plants",
-		    	img_path:"img/aac_board_imgs/bird.png"},
-
-		  { name:"Stuff",
-		    img_path:"img/aac_board_imgs/clock.png" },
-
-		  { name:"Things",
-		    img_path:"img/aac_board_imgs/crayon.png"},
-
-		    // also added for testing purposes
-		  { name:"Nouns",
-		    img_path:"img/aac_board_imgs/alpaca.png" },
-
-		  { name:"Outdoor",
-		    img_path:"img/aac_board_imgs/art.png" },
-
-		  { name:"Animals",
-		    img_path:"img/aac_board_imgs/balloon.png" },
-
-		  { name:"Plants",
-		    img_path:"img/aac_board_imgs/bird.png" },
-
-		  { name:"Stuff",
-		    img_path:"img/aac_board_imgs/clock.png" },
-
-		  { name:"Things",
-		    img_path:"img/aac_board_imgs/crayon.png"},
-
-		  { name:"Nouns",
-		    img_path:"img/aac_board_imgs/alpaca.png" },
-
-		  { name:"Outdoor",
-		    img_path:"img/aac_board_imgs/art.png" },
-
-		  { name:"Animals",
-		    img_path:"img/aac_board_imgs/balloon.png" },
-
-		  { name:"Plants",
-		    img_path:"img/aac_board_imgs/bird.png" },
-
-		  { name:"Stuff",
-		    img_path:"img/aac_board_imgs/clock.png" },
-
-		  { name:"Things",
-		    img_path:"img/aac_board_imgs/crayon.png"},
-
-		  { name:"Nouns",
-		    img_path:"img/aac_board_imgs/alpaca.png" },
-
-		  { name:"Outdoor",
-		    img_path:"img/aac_board_imgs/art.png" },
-
-		  { name:"Animals",
-		    img_path:"img/aac_board_imgs/balloon.png" },
-
-		  { name:"Plants",
-		    img_path:"img/aac_board_imgs/bird.png" },
-
-		  { name:"Stuff",
-		    img_path:"img/aac_board_imgs/clock.png" },
-
-		  { name:"Things",
-		    img_path:"img/aac_board_imgs/crayon.png"},
-		]
-
 		$scope.getData = function(){
 		  var req = {
 		    url: 'https://lexemes-dev.herokuapp.com/board/single/',
@@ -401,6 +246,162 @@ app.controller('boardFactoryController',
 		    console.log("This icon doesn't have an associated board");
 		  }
 		}
+
+
+		$scope.dummyBoards =[
+
+			{ name:"About Me",
+			  img_path:"img/aac_board_imgs/crayon.png",
+			  pk: '5'},
+
+		  { name:"Lunch",
+		    img_path:"img/aac_board_imgs/alpaca.png",
+		    pk: '3'},
+
+		  { name:"Feelings",
+		    img_path:"img/aac_board_imgs/art.png",
+		    pk: '4'},
+
+		  { name:"Body",
+		    img_path:"img/aac_board_imgs/balloon.png"},
+
+		  { name:"Clothes",
+		   	img_path:"img/aac_board_imgs/bird.png"},
+
+			{ name:"Cars",
+			  img_path:"img/aac_board_imgs/clock.png"},
+
+		  { name:"Feelings",
+		 		img_path:"img/aac_board_imgs/crayon.png"},
+
+			{ name:"Food",
+		    img_path:"img/aac_board_imgs/alpaca.png"},
+
+		  { name:"Kind Things",
+		    img_path:"img/aac_board_imgs/art.png"},
+
+		  { name:"Morning",
+		    img_path:"img/aac_board_imgs/balloon.png"},
+
+		  { name:"Number",
+		    img_path:"img/aac_board_imgs/bird.png"},
+
+			{ name:"People",
+		    img_path:"img/aac_board_imgs/clock.png"},
+
+		  { name:"Places",
+		    img_path:"img/aac_board_imgs/crayon.png"},
+
+		  { name:"Reading",
+		    img_path:"img/aac_board_imgs/alpaca.png"},
+
+		  { name:"Sports",
+		    img_path:"img/aac_board_imgs/art.png"},
+
+			{ name:"Things",
+		    img_path:"img/aac_board_imgs/balloon.png"},
+
+		  { name:"Time",
+		    img_path:"img/aac_board_imgs/bird.png"},
+
+		  { name:"Verbs",
+		    img_path:"img/aac_board_imgs/clock.png"},
+
+		  { name:"Weather",
+		    img_path:"img/aac_board_imgs/crayon.png"},
+
+			{ name:"Nouns",
+		    img_path:"img/aac_board_imgs/alpaca.png"},
+
+		  { name:"Outdoor",
+		    img_path:"img/aac_board_imgs/art.png"},
+
+		  { name:"Animals",
+		    img_path:"img/aac_board_imgs/balloon.png"},
+
+		  { name:"Plants",
+		    img_path:"img/aac_board_imgs/bird.png"},
+
+		  { name:"Stuff",
+		    img_path:"img/aac_board_imgs/clock.png"},
+
+		  { name:"Things",
+		    img_path:"img/aac_board_imgs/crayon.png"},
+
+		  // added for sliding option
+		  { name:"Nouns",
+		    img_path:"img/aac_board_imgs/alpaca.png"},
+
+		  { name:"Outdoor",
+		    img_path:"img/aac_board_imgs/art.png"},
+
+		  { name:"Animals",
+		    img_path:"img/aac_board_imgs/balloon.png"},
+
+		  { name:"Plants",
+		    	img_path:"img/aac_board_imgs/bird.png"},
+
+		  { name:"Stuff",
+		    img_path:"img/aac_board_imgs/clock.png" },
+
+		  { name:"Things",
+		    img_path:"img/aac_board_imgs/crayon.png"},
+
+		    // also added for testing purposes
+		  { name:"Nouns",
+		    img_path:"img/aac_board_imgs/alpaca.png" },
+
+		  { name:"Outdoor",
+		    img_path:"img/aac_board_imgs/art.png" },
+
+		  { name:"Animals",
+		    img_path:"img/aac_board_imgs/balloon.png" },
+
+		  { name:"Plants",
+		    img_path:"img/aac_board_imgs/bird.png" },
+
+		  { name:"Stuff",
+		    img_path:"img/aac_board_imgs/clock.png" },
+
+		  { name:"Things",
+		    img_path:"img/aac_board_imgs/crayon.png"},
+
+		  { name:"Nouns",
+		    img_path:"img/aac_board_imgs/alpaca.png" },
+
+		  { name:"Outdoor",
+		    img_path:"img/aac_board_imgs/art.png" },
+
+		  { name:"Animals",
+		    img_path:"img/aac_board_imgs/balloon.png" },
+
+		  { name:"Plants",
+		    img_path:"img/aac_board_imgs/bird.png" },
+
+		  { name:"Stuff",
+		    img_path:"img/aac_board_imgs/clock.png" },
+
+		  { name:"Things",
+		    img_path:"img/aac_board_imgs/crayon.png"},
+
+		  { name:"Nouns",
+		    img_path:"img/aac_board_imgs/alpaca.png" },
+
+		  { name:"Outdoor",
+		    img_path:"img/aac_board_imgs/art.png" },
+
+		  { name:"Animals",
+		    img_path:"img/aac_board_imgs/balloon.png" },
+
+		  { name:"Plants",
+		    img_path:"img/aac_board_imgs/bird.png" },
+
+		  { name:"Stuff",
+		    img_path:"img/aac_board_imgs/clock.png" },
+
+		  { name:"Things",
+		    img_path:"img/aac_board_imgs/crayon.png"},
+		]
 
 		var template = '<ion-popover-view class="popover-stuff2"><ion-content><p class="closing-x" ng-click="closePopover()">X</p><p class="popover-stuff">To edit the content of tiles with a yellow dot, go to Settings <a href="#/settings"><button class="custom-button"><i class="icon ion-gear-a"></i> Open Settings</button></a></p></ion-popover-view>';
 
