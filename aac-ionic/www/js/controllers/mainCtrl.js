0a71c08daf88ab4fcc3db7b4c11c5993419fc4fb
Luis J. Perez <luisjorge129@gmail.com>
2016-10-24 12:40:52 -0400

    removed AAC Service.

--- aac-ionic/www/js/controllers/mainCtrl.js
+++ aac-ionic/www/js/controllers/mainCtrl.js
@@ -27,8 +27,7 @@ app.filter('breaking2', function(){
 
 app.controller('mainController', 
   function($http, $scope, $ionicSideMenuDelegate, $ionicModal,
-    $location, $ionicPopover, $ionicHistory, appConfig,
-    aacService) {
+    $location, $ionicPopover, $ionicHistory, appConfig) {
     
     $ionicHistory.nextViewOptions({
       disableBack: true
@@ -46,8 +45,8 @@ app.controller('mainController',
       $scope.doLogout();
     };
 
-  $scope.columns = aacService.columns;
-  $scope.rows = aacService.rows;
+  $scope.columns = "abcdef";
+  $scope.rows = "123456";
   $scope.selectedTiles = [];
   $scope.selectedIndex = aacService.selectedIndex;
   $scope.titleLimit = aacService.titleLimit;
