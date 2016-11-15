ab2a009a426d967db4d16ac7e14c9b4ef6463ba8
Katie Cunningham <katie.fulton@gmail.com>
2016-11-15 11:34:43 -0500

    Board images are now saving. Next up, saving tile images, then bringing back offline

--- aac-ionic/www/js/controllers/mainCtrl.js
+++ aac-ionic/www/js/controllers/mainCtrl.js
@@ -55,24 +55,61 @@ function($http, $scope, $ionicSideMenuDelegate, $ionicModal,
     $scope.end = 24;
     $scope.board = {};
 
-    $scope.Download = function () {
+    function replaceImage (boardIndex, symbolIndex, targetPath, filename) {
+      console.log("Would be replacing",
+        $scope.userBoards[boardIndex].board.title,
+        targetPath, filename)
+      if (symbolIndex === null) {
+        // We're just replacing the board image
+        $cordovaFile.readAsDataURL(
+          cordova.file.applicationDirectory, "www/img/" + filename)
+          .then(function(res) {
+            console.log("Implanting apple on", boardIndex, $scope.userBoards[boardIndex].board.title)
+            $scope.userBoards[boardIndex].board.image = res;
+            $scope.userBoards[boardIndex].board.thumb = res;
+          })
+        }
+    }
+
+    $scope.Download = function (url, boardIndex, symbolIndex) {
+      if (url === null) {
+        return null
+      }
       ionic.Platform.ready(function(){
-       var url = "http://findicons.com/files/icons/1187/pickin_time/128/apple.png";
-       var filename = url.split("/").pop();
-       // var targetPath = '/tmp/aac/' + filename;
+        var filename = url.split("/").pop()
+        console.log("filename 1", filename)
+        filename = filename.split("?")[0]
+        console.log("filename 2", filename)
+        console.log(url.split("/"))
+        url = url.split("?")[0]
+
        var targetPath = cordova.file.applicationDirectory + "www/img/" + filename;
-
-
-
-        $cordovaFileTransfer.download(url, targetPath, {}, true).then(function (result) {
+        $cordovaFileTransfer.download(
+          url, 
+          targetPath, 
+          {}, 
+          true)
+        .then(function (result) {
               console.log('Save file on '+targetPath+' success!');
-              getFile(targetPath)
+              replaceImage(boardIndex, symbolIndex, targetPath, filename);
         }, function (error) {
               console.log('Error Download file', JSON.stringify(error));
         }, function (progress) {
               $scope.downloadProgress = (progress.loaded / progress.total) * 100;
         });
       });
+    }
+
+    function saveBoardImages () {
+      console.log("# boards", $scope.userBoards.length)
+      for (var i in $scope.userBoards) {
+        // Download board image
+        var board = $scope.userBoards[i].board
+        // console.log(JSON.stringify(board))
+        console.log("Downloading", board.title, 
+          board.image)
+        $scope.Download(board.image, i, null)
+      }
     }
 
     function listDir(path){
@@ -102,11 +139,10 @@ function($http, $scope, $ionicSideMenuDelegate, $ionicModal,
         function(err) {console.log("Nope")});
     }
 
-    $scope.Download()
-
     $scope.mainBoardLoader = function(){
       // Make sure we have to do this call! Are there boards already saved?
 
+      // if (window.localStorage.getItem('boards') === null) {
       if (true) {
         var req = {
           url: appConfig.backendURL + '/board/user/',
@@ -126,14 +162,14 @@ function($http, $scope, $ionicSideMenuDelegate, $ionicModal,
           $scope.quickbar = data.quickbar;
           $scope.filled_tiles = Object.keys($scope.board.symbols)
           $scope.downloadBoards(data)
-          console.log("Board ready, updating a thing ")
-
-          $cordovaFile.readAsDataURL(
-            cordova.file.applicationDirectory, "www/img/apple.png")
-            .then(function(res) {
-              $scope.board.symbols.a1.symbol.image = res; 
-              $scope.board.symbols.a1.symbol.thumb = res;  
-            })
+          console.log("Board ready, saving images")
+        })
+        .error(function(error) {
+          console.log("Error")
+          console.log(error)
+        })
+        .then(function () {
+          saveBoardImages();
         })
       } else {
         var data = angular.fromJson(window.localStorage['boards'])
@@ -513,6 +549,8 @@ function($http, $scope, $ionicSideMenuDelegate, $ionicModal,
       $scope.speakText = function(text) {
         TTS.speak({
           text: text,
+          locale: 'en-US', 
+          rate: 1.4
         }, function () {
           // Do Something after success
         }, function (reason) {
